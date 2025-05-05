c279937634bb760afd3fe9ac2cc34b8c6ffc01e3
Indrawahyu12 <rafaindra695@gmail.com>
2025-05-05 10:49:59 +0800

    Add files via upload

---
+++ MODUL-1/percobaan-1/percobaan-2/percobaan-3/percobaan-4/tuple.py
@@ -0,0 +1,29 @@
+# cara membuat tuple singleton
+satu = ('Isi',)
+dua = "ini adalah tuple?",
+
+# cek tipe datanya
+print(type(satu))
+print(type(dua))
+
+# jika tidak pakai koma
+satu = ('Isi')
+dua = "ini adalah tuple?"
+
+# cek tipe datanya
+print(type(satu))
+print(type(dua))
+
+
+# membuat tuple
+nama = ('muhammad', 'indra', 'wahyu')
+
+# mengakses indeks ke 1 dari tuple
+print(nama[1])
+
+
+# Membuat Tuple
+t = (1, 5, 10, 15)
+
+# mengubah isi elemen tuple
+t[0] = 0
